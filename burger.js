d960cbf1668b152f12a1d91ce3908a68fb0fe59b
Dytro <68156872+dytroInc@users.noreply.github.com>
2021-09-12 22:52:15 +0900

    Create burger.js

---
+++ burger.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('burger-menu').addEventListener('click', onClick)
+});
+function onClick() {
+    alert("아직 코딩 안 함 ㅋㅋㅋㅋ")
+}
